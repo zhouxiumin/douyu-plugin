3fa82e71c7d6fe01326e6187c87ab6e9a9378a89
zhouxiumin <zhouxiuminmeiwen@gmail.com>
2019-05-23 22:18:30 +0800

    layui

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -2,3 +2,13 @@
 
 console.log('\'斗鱼 \'斗鱼! Content script');
 
+layui.use('layer', function(){
+  let layer = layui.layer;
+
+  console.log('layer! Content script');
+  layer.open({
+    title: '在线调试'
+    ,content: '可以填写任意的layer代码'
+  });
+
+});
